ba4e39fe3801a76677a30594b597300a7ad4f711
blaytenshi <blay.tenshi@gmail.com>
2020-06-02 22:03:50 +1000

    Lesson 18 Union Types Part 3

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-function combine(input1, input2) {
+function combine(input1, input2, 
+// This is a literal type usually used in conjunction with union types
+// now TS will enforce either of these two strings as accepted values
+// It's still up to you to verify the value in your function
+resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
-var combinedNames = combine("jimmy", "shirley");
+var combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+var combinedNames = combine("jimmy", "shirley", 'as-text');
 console.log(combinedNames);
