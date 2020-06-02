a62ea38b0ab1474dde40a10ce33c876a4d51e600
blaytenshi <blay.tenshi@gmail.com>
2020-06-02 21:23:13 +1000

    Lesson 18 Union Types Part 1

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
-// you can also choose to start at a certain number so...
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 4] = "ADMIN";
-    Role[Role["READ_ONLY"] = 5] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 6] = "AUTHOR"; // 6
-})(Role || (Role = {}));
-var person = {
-    name: "Jimmy",
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.AUTHOR
-};
-var favouriteActivities;
-console.log(person.name);
-console.log("Role value", person.role); // should log out 6
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+function combine(input1, input2) {
+    var result = input1 + input2;
+    return result;
 }
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
--- app.ts
+++ app.ts
@@ -1,30 +1,8 @@
-// you can can even set it whatever you like but usually just numbers
-enum Role {
-  ADMIN = 4,
-  READ_ONLY = 100,
-  AUTHOR = 'AUTHOR',
-  // will throw an error because TS can't infer the next value after author
-  // USER
+// back to basic example
+function combine(input1: number, input2: number) {
+  const result = input1 + input2;
+  return result;
 }
 
-const person: {
-  name: string; 
-  age: number;
-  hobbies: string[];
-  role: Role; // give it the type!
-} = {
-  name: "Jimmy",
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  role: Role.AUTHOR
-}
-
-
-let favouriteActivities: string[];
-
-console.log(person.name);
-console.log("Role value", person.role); // should log out AUTHOR
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-}
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
