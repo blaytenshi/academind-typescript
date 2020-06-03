49b761499bf1720b887661651744613e3a7ca37c
blaytenshi <blay.tenshi@gmail.com>
2020-06-04 01:46:21 +1000

    Lesson 23 Callback Function Types Part 2

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-// Function types
-// back to our original add function
-function add(n1, n2) {
-    return n1 + n2;
+// callback functions as parameters
+// you declare the function type exactly as you would for a normal function!
+// the parameter that the callback function accepts is the result which is a
+// number type and because the actual function doesn't return anything, the 
+// return type is void!
+function addWithCallback(n1, n2, cb) {
+    var result = n1 + n2;
+    // don't need to redeclare the types here because it's already declared up top
+    cb(result);
 }
-// takes two number types and returns a number type
-// now in normal JS we can do this, assign the function to a variable and call it
-var combine = add;
-console.log(combine(5, 11));
+addWithCallback(10, 20, function (result) {
+    console.log(result); // should still log 30
+});
