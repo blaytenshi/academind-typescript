2e00d6203ea734b5c86ac75c1a99d8291297b12d
blaytenshi <blay.tenshi@gmail.com>
2020-06-09 18:18:17 +1000

    Lesson 49 let var and cost

---
+++ nextgenjs/dist/app.js
@@ -0,0 +1,15 @@
+"use strict";
+const sup = 'sup bitches';
+console.log(sup);
+let hello = 'General Kenobi';
+var hi = 'There!';
+function sayHi() {
+    var hi = 'Bye!';
+    console.log(hi);
+}
+sayHi();
+console.log(hi);
+if (hi === 'There!') {
+    var isOld = true;
+}
+//# sourceMappingURL=app.js.map
