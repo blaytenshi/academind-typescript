4f9d1d5a07c54e664780e928acffee7f84e1915b
Jimmy Tang <blay.tenshi@gmail.com>
2020-06-20 04:03:37 +1000

    Lesson 50 Default Function Parameters

--- nextgenjs/src/app.ts
+++ nextgenjs/src/app.ts
@@ -1,35 +1,34 @@
-// This is a JS constant, can't be changed
-const sup = 'sup bitches';
-// TS will give you an error
-// JS and Browser will throw an error, won't even log on the next line
-// sup = 'sup ninjas';
-console.log(sup);
-
-// This is the new standard for JS variables, can be changed
-let hello = 'General Kenobi';
-
-// var and let basically does the same thing in terms of scoping
-var hi = 'There!'; // globally declared
-
-function sayHi() {
-  var hi = 'Bye!'; // scope declared hi
-  console.log(hi); // will log 'Bye!' not 'There!'
+// below is a fairly standard js function
+const add = function (a: number, b: number) {
+    return a + b;
 }
 
-sayHi();
-
-console.log(hi);
-
-// there is a difference with let in that let only exists in block scope which
-// isn't a huge deal for functions but will be different in control statements
-// like ifs and whiles:
-// Note: you'll need to copy this chunk into the browser console because
-// TS will refuse to compile it
-if (hi === 'There!') {
-  var isOld = true; // globally declared!
+// below is an updated version of the same fuction but in arrow function format
+const tsAdd = (a: number, b: number) => {
+    return a + b;
 }
 
-// console.log(isOld); // should be undefined but will log true
+// you can omit the {} if you only have one line of code to execute and return
+// otherwise you need the curly braces
+const tsAddOneLine = (a: number, b: number) => a + b;
 
-// but if you use let then isOld will only be valid between the curly braces and
-// its children
+// now lets say we have a function that just console logs written in js
+// we can actually remove the () surrounding the parameters being passed into
+// the function when we only have one param being passed in:
+const printOutput = output => console.log(output);
+
+// the above ts will complain because there's no type for output. We can't 
+// directly add the type to output (say string or both string or number) so we 
+// have to surround it with () and add the typing. lets say it takes number 
+// and string:
+const tsPrintOutput = (output: number | string) => console.log(output);
+// now ts is happy
+
+// however it's still possible to put the typing in!
+// do you see what we did below? we've moved the type information to the 
+// variable that the function is assigned! newTsPringOutput now has a type
+// of a function that accepts an output variable that supports types number and
+// string and returns void (because we just console log)
+// have a careful look at it carefully to get used to the syntax!
+const newTsPrintOutput: (output: number | string) => void = output => console.log(output);
+//    ^fn Name          ^fn type                          ^assignment of the function itself!
