ad2c1113e86913459792f5a525827df9439f236e
Jimmy Tang <blay.tenshi@gmail.com>
2020-06-26 01:27:22 +1000

    Lesson 66 protected Modifier

--- 05classesinterfaces/dist/app.js
+++ 05classesinterfaces/dist/app.js
@@ -1,11 +1,11 @@
 "use strict";
 class Computer {
-    constructor(serialNumber, cpu = "intel", ram = "hynix", gpu = "nvidia") {
+    constructor(serialNumber, cpu = "intel", ram = "hynix", gpu = "nvidia", ssd = []) {
         this.serialNumber = serialNumber;
         this.cpu = cpu;
         this.ram = ram;
         this.gpu = gpu;
-        this.ssd = [];
+        this.ssd = ssd;
     }
     describe() {
         console.log(`serialNumber ${this.serialNumber} cpu ${this.cpu} hdd ${this.ram} gpu ${this.gpu}`);
@@ -13,11 +13,20 @@ class Computer {
     writeHdd(value) {
         this.ssd.push(value);
     }
+    readSsd() {
+        console.log(this.ssd);
+    }
 }
 class Macbook extends Computer {
     constructor(id, fingerprintReader) {
         super(id);
         this.fingerprintReader = fingerprintReader;
+    }
+    writeHdd(value) {
+        if (value === 'Apple sux') {
+            return;
+        }
+        this.ssd.push(value);
     }
 }
 class Surfacebook extends Computer {
@@ -30,4 +39,7 @@ const jimmysMacbook = new Macbook(5, true);
 const jimmysSurfacebook = new Surfacebook(1, false);
 console.log('jimmysmacbook', jimmysMacbook);
 console.log('jimmyssurfacebook', jimmysSurfacebook);
+jimmysMacbook.writeHdd('Apple sux');
+jimmysMacbook.writeHdd('Apple rulez');
+console.log(jimmysMacbook.readSsd());
 //# sourceMappingURL=app.js.map
--- 05classesinterfaces/src/app.ts
+++ 05classesinterfaces/src/app.ts
@@ -1,12 +1,10 @@
 class Computer {
-
-    private readonly ssd: string[] = [];
-
     constructor(
         private readonly serialNumber: number,
         private readonly cpu: string = "intel", 
         private readonly ram: string = "hynix", 
-        private readonly gpu: string = "nvidia"
+        private readonly gpu: string = "nvidia",
+        protected readonly ssd: string[] = []
     ) {
 
     }
@@ -17,6 +15,10 @@ class Computer {
 
     writeHdd(value: string) {
         this.ssd.push(value);
+    }
+
+    readSsd() {
+        console.log(this.ssd);
     }
 }
 
@@ -36,6 +38,19 @@ class Macbook extends Computer {
         // returned by super()
         this.fingerprintReader = fingerprintReader;
     }
+
+    writeHdd(value: string) {
+        if (value === 'Apple sux') {
+            return; // no dissin apple
+        }
+        // even though the macbook class extends from computer and has an ssd 
+        // field, it is currently private meaning you are only allowed to access
+        // it from within the Computer class. But since we need to access it here
+        // in a child class, we need to change the accessor on the field to
+        // protected which will allow you to access it fromn child classes. this
+        // applies to methods too!
+        this.ssd.push(value);
+    }
 }
 
 class Surfacebook extends Computer {
@@ -52,3 +67,8 @@ const jimmysSurfacebook = new Surfacebook(1, false);
 
 console.log('jimmysmacbook', jimmysMacbook);
 console.log('jimmyssurfacebook', jimmysSurfacebook);
+
+jimmysMacbook.writeHdd('Apple sux');
+jimmysMacbook.writeHdd('Apple rulez');
+
+console.log(jimmysMacbook.readSsd());
